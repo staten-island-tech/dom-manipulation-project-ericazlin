72e180f656be5a38e9d8b92fbabafb3d54bbc11e
ericazlin <113201249+ericazlin@users.noreply.github.com>
2022-11-07 13:15:08 -0500

    why do i have so much work even though there is 3 days of school this week grgagrgagagrgaga

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const DOMSelectors = {
   
   DOMSelectors.box.insertAdjacentHTML(
     "beforebegin",
-    `We can add text into an HTML file by writing it in JS!`
   );
   
   DOMSelectors.button.addEventListener("click", function () {
@@ -23,9 +22,6 @@ const DOMSelectors = {
   });
   
   DOMSelectors.button.insertAdjacentHTML(
-    "afterend",
-    `<p>you can have multiple HTML elements in the same DOMSelector!<p>
-    <img src="https://i.imgur.com/zQ5ds03.jpeg" class="cat-poster"></img>`
   );
   
   
