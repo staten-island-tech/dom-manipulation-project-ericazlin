b98314db1792679192289a641b56b4e65ec2f06a
ericazlin <113201249+ericazlin@users.noreply.github.com>
2022-11-10 12:40:14 -0500

    i took part two of my physics test and lowkey threw but whatever cause 3 day weekend amirite

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
 const DOMSelectors = {
-    button: document.getElementById("btn"),
-    box: document.getElementById("container-box"),
-    input: document.querySelector(`#input`),
-  };
   
-  DOMSelectors.box.insertAdjacentHTML(
-    "beforebegin",
-  );
-  
-  DOMSelectors.button.addEventListener("click", function () {
-    let input = DOMSelectors.input.value; 
-  
-    console.log(input);
-  
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `
-          <p class = 'extra-text'>${input}</p>
-      `
-    );
-  });
-  
-  DOMSelectors.button.insertAdjacentHTML(
-  );
-  
-  
+}
