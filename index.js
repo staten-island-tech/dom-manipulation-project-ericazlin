6575a04b3dd416eaab465a8ad380cb1cf22fe121
ericazlin <ericazlin@gmail.com>
2022-11-15 00:33:55 -0500

    whatever yo

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // defining DOMSelectors
 const DOMSelectors = {
-     insertbirthyear: document.getElementById("insertbirthyear"),
-     calculate: document.getElementById("calculate"),
-     reset: document.getElementById("reset"),
-     box: document.getElementById("box"),
-     clear: document.getElementById("clear")
-  }
-  
-  
-  DOMSelectors.submit.addEventListener("click", function (insertbirthyear) {
-    
-  })
+   box: document.getElementById("box"),
+   calculate: document.getElementById("btnCalc"),
+   clear: document.getElementById("btnClear"),
+   title: document.getElementById("title"),
+   ageYear: document.getElementById("ageYear"),
+   button: document.querySelector(".btn"),
+}
+
+function ageCalc(year) {
+   const currentYear = 2022;
+   let input = year;
+   return age = currentYear - input
+}
+
+function ageClear(){
+   DOMSelectors.ageYear.value = ""
+}
+
+DOMSelectors.calculate.addEventListener("click", function () {
+   let age = ageCalc(DOMSelectors.ageYear.value);
+   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>You are ${age} years old!</p>`)
+})
+
+DOMSelectors.clear.addEventListener("click", function () {
+   ageClear();
+})
+
