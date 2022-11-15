c6bbb6e86afdc8535490a05b32a72382053bd2ed
ericazlin <ericazlin@gmail.com>
2022-11-14 23:21:31 -0500

    i ifnished writing my dbq sheet yay

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// defining DOMSelectors
 const DOMSelectors = {
+     insertbirthyear: document.getElementById("insertbirthyear"),
+     calculate: document.getElementById("calculate"),
+     reset: document.getElementById("reset"),
+     box: document.getElementById("box"),
+     clear: document.getElementById("clear")
+  }
   
-}
+  
+  DOMSelectors.submit.addEventListener("click", function (insertbirthyear) {
+    
+  })
