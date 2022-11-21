241690f78a26ad12f794b48cc290a607f3699d0a
ericazlin <ericazlin@gmail.com>
2022-11-21 00:40:07 -0500

    yay another 3 day week and then the big turkey day

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 // defining DOMSelectors
 const DOMSelectors = {
    box: document.getElementById("box"),
-   calculate: document.getElementById("btnCalc"),
+   create: document.getElementById("btnCreate"),
    clear: document.getElementById("btnClear"),
    title: document.getElementById("title"),
-   ageYear: document.getElementById("ageYear"),
+   releaseYear: document.getElementById("releaseYear"),
+   titleTrack: document.getElementById("titleTrack"),
+   artistName: document.getElementById("artistName"),
+   imageLink: document.getElementById("imageLink"),
+   albumName: document.querySelector("#albumName"),
    button: document.querySelector(".btn"),
+   albums: document.getElementById("albums"),
 }
 
-function
+const inputFields = [
+   inputYear = document.getElementById("releaseYear"),
+   inputTrack = document.getElementById("titleTrack"),
+   inputArtist = document.getElementById("artistName"),
+   inputImage = document.getElementById("imageLink"),
+   inputName = document.querySelector("#albumName"),
+]
 
-DOMSelectors.calculate.addEventListener("click", function () {
-   let age = ageCalc(DOMSelectors.ageYear.value);
-   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>You are ${age} years old!</p>`)
+function inputClear() {
+   inputFields.forEach(input => {
+      input.value = ""
+   });
+}
+
+DOMSelectors.create.addEventListener("click", function () {
+   let name = DOMSelectors.albumName.value;
+   let artist = DOMSelectors.artistName.value;
+   let title = DOMSelectors.titleTrack.value;
+   let year = DOMSelectors.releaseYear.value;
+   let image = DOMSelectors.imageLink.value;
+
+   DOMSelectors.box.insertAdjacentHTML("afterend", `
+   <div id="albums">    
+   </div>
+   `)
+
+   albums.insertAdjacentHTML("beforeend",
+   `<img src="${image}" alt=""> 
+   <h1>${name} - ${artist}<h1>
+   <h1>${title} - ${year}<h1> 
+   `
+   )
+
 })
 
 DOMSelectors.clear.addEventListener("click", function () {
-   ageClear();
+   inputClear();
+   albums.remove();
 })
 
